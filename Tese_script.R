9a1b15b7c040ad7042d542ba00055a655c181116
Eddac <cordeiro.emanuel@gmail.com>
2021-12-23 18:50:44 -0300

    Dados

--- Tese_script.R
+++ Tese_script.R
@@ -7,9 +7,10 @@ library(expss)
 library(RColorBrewer)
 
 # Carregando banco de dados no R
-Dados_ser_ <- readxl::read_xlsx("HSE_SERSO.xlsx")
+Dados_ser_ <- readxl::read_xlsx("HSE_DOC_SERSO_2.xlsx")
 Dados_AP_ <- readxl::read_xlsx("HSE_AP.xlsx") 
-Dados_mau_ <- readxl::read_xlsx("HSE_DOC_MAU.xlsx")
+Dados_mau_ <- readxl::read_xlsx("HSE_DOC_MAU_2.xlsx")
+Dados_Odo <- readxl::read_xlsx("HSE_DOC_Odonto.xlsx")
 
 # retirada da linha 33 por ser repetida com a linha 35 (Natalia)
 Dados_AP_ <- Dados_AP_ %>% slice(-33)
@@ -33,6 +34,7 @@ Dados_mau_2 <- Dados_mau_ %>% rename("1ª Amizade" = "1ª Proximidade",
                                    "3ª Amizade" = "3ª Proximidade",
                                    "4ª Amizade" = "4ª Proximidade",
                                    "5ª Amizade" = "5ª Proximidade")
+
 
 # Unir todos os bancos----
 df_geral <- rbind(Dados_AP_2, Dados_ser, Dados_mau_2)
@@ -101,15 +103,24 @@ plot(AP, vertex.color = "gold", vertex.label.color = "black", vertex.label.cex =
 AP
 V(AP)$name
 E(AP)$weight <- ars_ap_all_2$Peso
-V(AP)$gender <- c("M", "M", "M", "F", "F", "F", "F", "F", "M", "F", "M", "F", "M", "F", "M", "F", "F", "M", 
-                  "F", "F", "F", "M", "M", "F", "F", "F", "M", "F", "M", "F", "F", "M", "F", "M", "F", "F", "F")
+V(AP)$gender <- c("M", "M", "M", "F", 
+                  "F", "F", "F", "F", 
+                  "M", "F", "M", "F", 
+                  "M", "F", "M", "F", 
+                  "F", "M", "F", "F", 
+                  "F", "M", "M", "F", 
+                  "F", "F", "M", "F",
+                  "M", "F", "F", "M", 
+                  "F", "M", "F", "F", 
+                  "F")
 
 # Atributo dos sujeitos sem nomes
 V(AP)$suj <- paste("s", 1:37, sep = "")
 
 # Nível de centralidade
 AP_deg <- degree(AP, mode = c("in"))
-AP_deg
+
+AP_deg <- as.data.frame(AP_deg)
 
 # EIGENVECTOR CENTRALITY
 AP_eig <- evcent(AP)$vector
@@ -148,6 +159,76 @@ plot(AP,
      edge.width = E(AP)$weight, 
      layout = layout_components)
 
+
+
+## Análise de distanciamento Psicologia
+ars_ap_d_1 <- df_ap %>% select(1,7) %>% rename(Distância = "1ª distanciamento")
+ars_ap_d_1$Peso <- 5
+ars_ap_d_2 <- df_ap %>% select(1,8) %>% rename(Distância = "2ª distanciamento")
+ars_ap_d_2$Peso <- 4
+ars_ap_d_3 <- df_ap %>% select(1,9) %>% rename(Distância = "3ª distanciamento")
+ars_ap_d_3$Peso <- 3
+ars_ap_d_4 <- df_ap %>% select(1,10) %>% rename(Distância = "4ª distanciamento")
+ars_ap_d_4$Peso <- 2
+ars_ap_d_5 <- df_ap %>% select(1,11) %>% rename(Distância = "5ª distanciamento")
+ars_ap_d_5$Peso <- 1
+
+ars_ap_d_all <- rbind(ars_ap_d_1,
+                    ars_ap_d_2,
+                    ars_ap_d_3,
+                    ars_ap_d_4,
+                    ars_ap_d_5)
+
+ars_ap_d_all <- na.omit(ars_ap_d_all)
+
+AP_d <- graph_from_data_frame(ars_ap_d_all, directed = TRUE, vertices = NULL)
+
+
+plot(AP_d, vertex.color = "gold", vertex.label.color = "black", vertex.label.cex = 0.8, edge.arrow.size=.4,
+     edge.curved=0.2)
+
+
+E(AP_d)$weight <- ars_ap_d_all$Peso
+V(AP_d)$gender <- c("M", "M", "M", "F", 
+                  "F", "F", "F", "F", 
+                  "M", "F", "M", "F", 
+                  "M", "F", "M", "F", 
+                  "F", "M", "F", "F", 
+                  "F", "M", "M", "F", 
+                  "F", "F", "M", "F",
+                  "M", "F", "F", "M", 
+                  "F", "M", "F", "F", 
+                  "F")
+
+
+# Nível de centralidade
+AP_d_deg <- degree(AP_d, mode = c("in"))
+AP_d_deg
+
+# EIGENVECTOR CENTRALITY
+AP_d_eig <- evcent(AP_d)$vector
+
+# Betweenness centrality
+AP_d_bw <- betweenness(AP_d, directed = TRUE)
+
+# Colocando as métricas no grafo
+V(AP_d)$degree <- AP_d_deg
+V(AP_d)$eig <- AP_d_eig
+V(AP_d)$bw <- AP_d_bw
+
+
+plot(AP_d, 
+     vertex.color = c("gold", "skyblue")[1+(V(AP_d)$gender=="M")], 
+     #vertex.label = V(AP)$suj,
+     vertex.label.color = "black", 
+     vertex.label.cex = 1, 
+     #vertex.size = sqrt(V(AP)$bw),
+     edge.arrow.size=.1,
+     edge.curved=0.2, 
+     edge.width = E(AP_d)$weight, 
+     layout = layout.fruchterman.reingold)
+
+## Análise profissional Psicologia
 # Usar o pacote arsenal
 
 ## retirar os escores e tirar as precisões
